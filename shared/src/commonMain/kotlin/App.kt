f519fc924be3ddfdc8a937a0f50752bbc2dc3c12
Name <ahmad.awaab@gmail.com>
2023-04-09 00:44:50 -0400

    Initial project template

---
+++ shared/src/commonMain/kotlin/App.kt
@@ -0,0 +1,36 @@
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun App() {
+    MaterialTheme {
+        Scaffold(
+            topBar = { CenterAlignedTopAppBar(title = { Text("Hello World") }) },
+            bottomBar = {
+                NavigationBar {
+                    NavigationBarItem(
+                        selected = true,
+                        onClick = {},
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Outlined.Home,
+                                contentDescription = null
+                            )
+                        }
+                    )
+                }
+            }
+        ) {
+            Text(modifier = Modifier.padding(it), text = "Hello ${getPlatformName()}")
+        }
+    }
+}
+
+expect fun getPlatformName(): String
