6dc8068375f8d3849c3caa400cf97ea9524dc004
Name <ahmad.awaab@gmail.com>
2023-09-16 21:24:17 -0400

    Added supermemo usecase

---
+++ shared/src/commonMain/kotlin/com/github/ahmad_hossain/quranhifzrevision/feature_pages/data/domain/SuperMemo.kt
@@ -0,0 +1,39 @@
+package com.github.ahmad_hossain.quranhifzrevision.feature_pages.data.domain
+
+import com.github.ahmadhossain.quranhifzrevision.Page
+import kotlin.math.roundToInt
+
+/** Utilizes the SuperMemo 2 algorithm to calculate a Page's new interval, repetition, and eFactor */
+object SuperMemo {
+
+    operator fun invoke(
+        page: Page,
+        grade: Int,
+    ): Page {
+        val nextInterval: Int
+        val nextRepetition: Int
+        var nextEfactor: Double
+
+        if (grade >= 3) {
+            nextInterval = when (page.repetitions) {
+                0 -> 1
+                1 -> 6
+                else -> (page.interval * page.eFactor).roundToInt()
+            }
+            nextRepetition = page.repetitions + 1
+        } else {
+            nextInterval = 1
+            nextRepetition = 0
+        }
+
+        nextEfactor = page.eFactor + (0.1 - (5 - grade) * (0.08 + (5 - grade) * 0.02))
+
+        if (nextEfactor < 1.3) nextEfactor = 1.3
+
+        return page.copy(
+            interval = nextInterval,
+            repetitions = nextRepetition,
+            eFactor = nextEfactor,
+        )
+    }
+}
