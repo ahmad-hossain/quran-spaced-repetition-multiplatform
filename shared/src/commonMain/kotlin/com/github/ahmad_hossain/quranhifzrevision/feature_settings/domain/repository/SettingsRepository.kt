a06b6e90eee4cb52cbd9ca929fd52e14bd329769
Name <ahmad.awaab@gmail.com>
2023-10-01 13:30:27 -0400

    Refactored SettingsRepository for ease-of-use & readability

--- shared/src/commonMain/kotlin/com/github/ahmad_hossain/quranhifzrevision/feature_settings/domain/repository/SettingsRepository.kt
+++ shared/src/commonMain/kotlin/com/github/ahmad_hossain/quranhifzrevision/feature_settings/domain/repository/SettingsRepository.kt
@@ -3,7 +3,6 @@ package com.github.ahmad_hossain.quranhifzrevision.feature_settings.domain.repos
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.IOException
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
@@ -27,31 +26,37 @@ class SettingsRepository(
             }
         }.map { it.toUserPreferences() }
 
-    suspend fun getData() {
-        dataStore.data.first().toPreferences()
-    }
+    suspend fun getUserPreferences() = dataStore.data.first().toUserPreferences()
 
-    suspend fun setData(userPreferences: UserPreferences) {
-        dataStore.edit {
-            it[START_PAGE] = userPreferences.startPage
-            it[END_PAGE] = userPreferences.endPage
-            it[NOTIFICATION_TIME] = userPreferences.notificationTime.toString()
+    suspend fun updateDatastore(update: (UserPreferences) -> UserPreferences) {
+        dataStore.updateData { prefs ->
+            val updated = update(prefs.toUserPreferences())
+            updated.toPreferences()
         }
-    }
-
-    private fun Preferences.toUserPreferences(): UserPreferences {
-        val defaultPref = UserPreferences()
-        val startPage = this[START_PAGE] ?: defaultPref.startPage
-        val endPage = this[END_PAGE] ?: defaultPref.endPage
-        val notificationTime = LocalTime.parse(
-            this[NOTIFICATION_TIME] ?: defaultPref.notificationTime.toString()
-        )
-        return UserPreferences(startPage, endPage, notificationTime)
     }
 
     private companion object {
         val START_PAGE = intPreferencesKey("start_page")
         val END_PAGE = intPreferencesKey("end_page")
         val NOTIFICATION_TIME = stringPreferencesKey("notification_time")
+
+        fun Preferences.toUserPreferences(): UserPreferences {
+            val defaultPref = UserPreferences()
+            val startPage = this[START_PAGE] ?: defaultPref.startPage
+            val endPage = this[END_PAGE] ?: defaultPref.endPage
+            val notificationTime = LocalTime.parse(
+                this[NOTIFICATION_TIME] ?: defaultPref.notificationTime.toString()
+            )
+            return UserPreferences(startPage, endPage, notificationTime)
+        }
+
+        fun UserPreferences.toPreferences(): Preferences {
+            val userPrefs = this
+            return emptyPreferences().toMutablePreferences().apply {
+                this[START_PAGE] = userPrefs.startPage
+                this[END_PAGE] = userPrefs.endPage
+                this[NOTIFICATION_TIME] = userPrefs.notificationTime.toString()
+            }
+        }
     }
 }
