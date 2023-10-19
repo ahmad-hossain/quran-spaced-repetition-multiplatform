08303552f53f5ff36e40eb476cea128ab8545c3d
Name <ahmad.awaab@gmail.com>
2023-10-19 09:39:03 -0400

    Added nav-bar title to settings-screen

--- iosApp/iosApp/SettingsView.swift
+++ iosApp/iosApp/SettingsView.swift
@@ -31,58 +31,61 @@ struct SettingsView: View {
     let strings = SharedRes.strings()
 
     var body: some View {
-        Form {
-            let prefs = viewModel.stateValue.userPreferences
+        NavigationView {
+            Form {
+                let prefs = viewModel.stateValue.userPreferences
 
-            Section(header: Text(getString(strings.revision))) {
-                HStack {
-                    Text(getString(strings.quran_pages_setting))
-                    Spacer()
-                    let buttonBgColor = colorScheme == .dark ? UIColor.systemGray4 : UIColor.systemGray5
-                    let buttonBgOpacity = colorScheme == .dark ? 1 : 0.6
-                    // TODO: handle on-click
-                    Button("\(prefs.startPage) - \(prefs.endPage)") {}
-                        .accentColor(.primary)
-                        .padding(.horizontal, 11)
-                        .padding(.vertical, 8)
-                        .background(Color(buttonBgColor).opacity(buttonBgOpacity))
-                        .cornerRadius(7)
-                        .buttonStyle(.borderless)
+                Section(header: Text(getString(strings.revision))) {
+                    HStack {
+                        Text(getString(strings.quran_pages_setting))
+                        Spacer()
+                        let buttonBgColor = colorScheme == .dark ? UIColor.systemGray4 : UIColor.systemGray5
+                        let buttonBgOpacity = colorScheme == .dark ? 1 : 0.6
+                        // TODO: handle on-click
+                        Button("\(prefs.startPage) - \(prefs.endPage)") {}
+                            .accentColor(.primary)
+                            .padding(.horizontal, 11)
+                            .padding(.vertical, 8)
+                            .background(Color(buttonBgColor).opacity(buttonBgOpacity))
+                            .cornerRadius(7)
+                            .buttonStyle(.borderless)
+                    }
+                    .padding(.vertical, -1)
                 }
-                .padding(.vertical, -1)
-            }
 
-            Section(header: Text(getString(strings.notifications))) {
-                DatePicker(
-                    getString(strings.notification_time_setting),
-                    selection: notificationDateBinding,
-                    displayedComponents: .hourAndMinute
-                )
-            }
+                Section(header: Text(getString(strings.notifications))) {
+                    DatePicker(
+                        getString(strings.notification_time_setting),
+                        selection: notificationDateBinding,
+                        displayedComponents: .hourAndMinute
+                    )
+                }
 
-            Section(
-                header: Text(getString(strings.backup)),
-                footer: Text(getString(strings.backup_footer))
-            ) {
-                Button(
-                    action: {},
-                    label: {
-                        Label(
-                            getString(strings.headline_export_data),
-                            systemImage: "square.and.arrow.up"
-                        )
-                    }
-                )
-                Button(
-                    action: {},
-                    label: {
-                        Label(
-                            getString(strings.headline_import_data),
-                            systemImage: "square.and.arrow.down"
-                        )
-                    }
-                )
+                Section(
+                    header: Text(getString(strings.backup)),
+                    footer: Text(getString(strings.backup_footer))
+                ) {
+                    Button(
+                        action: {},
+                        label: {
+                            Label(
+                                getString(strings.headline_export_data),
+                                systemImage: "square.and.arrow.up"
+                            )
+                        }
+                    )
+                    Button(
+                        action: {},
+                        label: {
+                            Label(
+                                getString(strings.headline_import_data),
+                                systemImage: "square.and.arrow.down"
+                            )
+                        }
+                    )
+                }
             }
+            .navigationBarTitle(getString(strings.settings), displayMode: .inline)
         }
     }
 }
