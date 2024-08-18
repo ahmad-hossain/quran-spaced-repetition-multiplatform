a8752dc925636597ed6ac3f596267b662fbeadfb
Name <ahmad.awaab@gmail.com>
2024-08-18 12:25:29 -0700

    Added compose-ui-tooling dep to enable compose previews for Android

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.compose.bom)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.material.icons.extended)
